b7118639218a6f590d4329e5df2995b1146b1070
davidfekke <david@fekke.com>
2016-11-27 17:36:34 -0500

    Adding Tab view

---
+++ NextMeeting.js
@@ -0,0 +1,57 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+var NextMeeting = React.createClass({
+    getInitialState () {
+        fetch('https://www.jaxnode.com/v1/api/meeting')
+            .then(response => {
+                return response.json();
+            })
+            .then(json => {
+                console.log(json);
+                this.setState(Object.assign({}, this.state, { meeting: json.meeting }));
+            });
+        return {
+            meeting: { name: 'Next Meeting' }
+        };
+    },
+    render: function() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    {this.state.meeting.name}
+                </Text>
+                <Text style={styles.description}>
+                    Use this app to find out when the next meeting will be, get directions, find links to our sponsors and github code.
+                </Text>
+            </View>
+        );
+    }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  description: {
+      fontSize: 15,
+      textAlign: 'center',
+      margin: 10
+  }
+});
+
+export default NextMeeting;
